8c93511b4bd505a8e3759c97ba1f862c40515127
massivebugs <massivebugs@protonmail.com>
2024-11-26 19:57:50 -0400

    animation change

--- app/components/atoms/SandmanText.tsx
+++ app/components/atoms/SandmanText.tsx
@@ -1,43 +1,44 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function SandmanText(props: {
   text: string;
   initialPos: "left" | "right";
   restore: boolean;
 }) {
-  const [showSplit, setShowSplit] = useState<boolean>(true);
-  const onTransitionEnd = (idx: number) => {
-    if (idx === props.text.length - 1) {
-      setShowSplit(false);
+  const [currentText, setCurrentText] = useState<string>("");
+  const animationHandler = useRef<number>();
+
+  useEffect(() => {
+    if (!props.restore) {
+      return;
     }
-  };
-  return showSplit
-    ? props.text.split("").map((v, idx) => (
-        <span
-          onTransitionEnd={() => onTransitionEnd(idx)}
-          className="whitespace-pre inline-block transition-transform"
-          style={{
-            display: props.restore ? undefined : "hidden",
-            transitionProperty: "transform, opacity",
-            transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
-            transform: props.restore
-              ? ""
-              : props.initialPos === "left"
-              ? "translate(-10px,-30px)"
-              : "translate(10px,-30px)",
-            opacity: props.restore ? "" : "0",
-            transitionDelay:
-              props.initialPos === "left"
-                ? `${(props.text.length - idx) * 5}ms`
-                : `${idx * 5}ms`,
-            transitionDuration: "300ms",
-          }}
-          key={idx}
-        >
-          {v}
-        </span>
-      ))
-    : props.text;
+
+    let buffer = "";
+    let currentTextIdx = 0;
+    let prevTime: number | null = null;
+    const animate: FrameRequestCallback = (time) => {
+      if (prevTime === null) {
+        prevTime = time;
+      }
+
+      const delta = time - prevTime;
+
+      if (delta > 5) {
+        buffer += props.text[currentTextIdx];
+        setCurrentText(buffer);
+        currentTextIdx++;
+        prevTime = time;
+      }
+
+      if (!(currentTextIdx === props.text.length)) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    animationHandler.current = requestAnimationFrame(animate);
+  }, [props.restore]);
+
+  return <>{currentText}</>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,32 +13,6 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      keyframes: {
-        "fly-in-from-right": {
-          "0%": {
-            // transform: "translate(100vw, -100vh)",
-            // animationTimingFunction: "cubic-bezier(0.02, 0.01, 0.21, 1)",
-            offsetDistance: "0%",
-          },
-          "100%": {
-            // transform: "translate(0px, 0px)",
-            // animationTimingFunction: "cubic-bezier(0.02, 0.01, 0.21, 1)",
-            offsetDistance: "75%",
-          },
-        },
-        x: {
-          "0%": {
-            transform: "translate(100vw, -100vh)",
-          },
-          "100%": {
-            transform: "translate(0px, 0px)",
-          },
-        },
-      },
-      animation: {
-        "fly-in-from-right": "fly-in-from-right 1s infinite",
-        foo: "x 3s ease-in-out infinite",
-      },
     },
   },
   plugins: [],
